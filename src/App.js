3aa1d0d1c4d3334a6b9d9ae368d4bb8efed17223
Abdul Vahid K P <abdulvahid1117@gmail.com>
2023-01-01 08:40:15 +0530

    change compenent according to our route search but refreshing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Profile from './components/Profile';
+import {BrowserRouter,Routes ,Route} from 'react-router-dom'
 
 function App() {
-  const [state, setState] = useState('')
-  let component;
-  if(state === 'about'){
-    component = <About />
-  }
-  if(state === 'profile') {
-    component = <Profile />
-  }
+ 
   return (
-    <div className="App">
-      <button onClick={()=>setState('about')}>about</button>
-      <button onClick={()=>setState('profile')}>Profile</button>
-      {component}
-        
-    </div>
+      <BrowserRouter>
+      <Routes>
+        <Route element={<About/>} path='/about' />
+        <Route element={<Profile/>} path='profile' />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
