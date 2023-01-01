69c570233e674af40e41de6b6386925b771ef3d3
Abdul Vahid K P <abdulvahid1117@gmail.com>
2023-01-01 07:44:39 +0530

    component changed using if without using root

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import About from './components/About';
+import Profile from './components/Profile';
 
 function App() {
+  const [state, setState] = useState('')
+  let component;
+  if(state === 'about'){
+    component = <About />
+  }
+  if(state === 'profile') {
+    component = <Profile />
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=>setState('about')}>about</button>
+      <button onClick={()=>setState('profile')}>Profile</button>
+      {component}
+        
     </div>
   );
 }
